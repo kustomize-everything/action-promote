cd7c3bd8b9b15575bd4444e9c6f87ff69dc2e3ab
Brandon High <highb@users.noreply.github.com>
2023-03-06 15:20:10 -0800

    fail unset

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Fail on non-zero exit code
+set -e
+
+# Fail on unset variables
+set -o nounset
+
 # `$#` expands to the number of arguments and `$@` expands to the supplied `args`
 printf '%d args:' "$#"
 printf " '%s'" "$@"
