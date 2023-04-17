da8d781b765e5aa72c68e91d27a381f22a93c18f
Brandon High <highb@users.noreply.github.com>
2023-04-17 16:09:52 -0700

    use -r (--raw) option on JQ to remove quotes

--- src/commit-and-pull-request.sh
+++ src/commit-and-pull-request.sh
@@ -80,15 +80,15 @@ fi
 if [[ "${PROMOTION_METHOD}" == "pull_request" ]]; then
   if [[ "${AGGREGATE_PR_CHANGES}" == "true" ]]; then
     BRANCH_REGEX=$(echo "promotion/${GITHUB_REPOSITORY:?}/${TARGET_BRANCH:?}/${PR_UNIQUE_KEY:?}"|tr "/" "-")
-    HEAD_REF_NAME=$(gh pr list --json headRefName | jq -c '.[].headRefName')
+    HEAD_REF_NAME=$(gh pr list --json headRefName | jq -rc '.[].headRefName')
     if [[ "${HEAD_REF_NAME}" =~ .*${BRANCH_REGEX}.* ]]; then
-      BRANCH=$(gh pr list --json headRefName | jq -c '.[].headRefName' | grep "${BRANCH_REGEX}")
+      BRANCH=$(gh pr list --json headRefName | jq -rc '.[].headRefName' | grep "${BRANCH_REGEX}")
       git stash
       git checkout -B "${BRANCH}"
       git rebase "${TARGET_BRANCH}"
       git stash apply
     else
-      BRANCH="$(echo "promotion/${GITHUB_REPOSITORY:?}/${TARGET_BRANCH:?}/${PR_UNIQUE_KEY:?}/${GITHUB_SHA:?}" | tr "/" "-")"
+      BRANCH=$(echo "promotion/${GITHUB_REPOSITORY:?}/${TARGET_BRANCH:?}/${PR_UNIQUE_KEY:?}/${GITHUB_SHA:?}" | tr "/" "-")
       git checkout -B "${BRANCH}"
     fi
   else
