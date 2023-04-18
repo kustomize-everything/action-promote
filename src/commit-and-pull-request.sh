556a45f8e7414f16901aa824ff32e090d7210528
Navneet Bairwal <50173691+navneet-bairwal@users.noreply.github.com>
2023-04-18 23:04:54 +0530

    Adding overlay name to Branch name (#26)
    Co-authored-by: Brandon High <highb@users.noreply.github.com>

--- src/commit-and-pull-request.sh
+++ src/commit-and-pull-request.sh
@@ -79,7 +79,7 @@ fi
 
 if [[ "${PROMOTION_METHOD}" == "pull_request" ]]; then
   if [[ "${AGGREGATE_PR_CHANGES}" == "true" ]]; then
-    BRANCH_REGEX=$(echo "promotion/${GITHUB_REPOSITORY:?}/${TARGET_BRANCH:?}/${PR_UNIQUE_KEY:?}"|tr "/" "-")
+    BRANCH_REGEX=$(echo "promotion/${GITHUB_REPOSITORY:?}/${TARGET_BRANCH:?}/${OVERLAY_NAMES_NO_SLASH:?}/${PR_UNIQUE_KEY:?}"|tr "/" "-")
     HEAD_REF_NAME=$(gh pr list --json headRefName | jq -rc '.[].headRefName')
     if [[ "${HEAD_REF_NAME}" =~ .*${BRANCH_REGEX}.* ]]; then
       BRANCH=$(gh pr list --json headRefName | jq -rc '.[].headRefName' | grep "${BRANCH_REGEX}")
@@ -88,7 +88,7 @@ if [[ "${PROMOTION_METHOD}" == "pull_request" ]]; then
       git rebase "${TARGET_BRANCH}"
       git stash apply
     else
-      BRANCH=$(echo "promotion/${GITHUB_REPOSITORY:?}/${TARGET_BRANCH:?}/${PR_UNIQUE_KEY:?}/${GITHUB_SHA:?}" | tr "/" "-")
+      BRANCH=$(echo "promotion/${GITHUB_REPOSITORY:?}/${TARGET_BRANCH:?}/${OVERLAY_NAMES_NO_SLASH:?}/${PR_UNIQUE_KEY:?}/${GITHUB_SHA:?}" | tr "/" "-")
       git checkout -B "${BRANCH}"
     fi
   else
