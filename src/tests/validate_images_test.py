af6027e15300951ecf93c59dca58948129324b42
Brandon High <highb@users.noreply.github.com>
2023-10-02 18:03:13 -0700

    Just make a file

--- src/tests/validate_images_test.py
+++ src/tests/validate_images_test.py
@@ -2,7 +2,7 @@ import pytest
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from helpers.validate import validate_images
+from validate import validate_images
 
 @pytest.mark.parametrize("images, expected", [
   # Basic
